6dd68f82a768cb024582d7b75c149aaff2dccb18
chenzhiqiang <chenzhiqiangit@126.com>
2017-03-30 13:21:42 +0800

    修改项目

---
+++ orange-service/src/main/java/com/orange/service/user/UserService.java
@@ -0,0 +1,7 @@
+package com.orange.service.user;
+
+/**
+ * Created by chzq on 2017/3/30.
+ */
+public interface UserService {
+}
---
+++ orange-serviceImpl/src/main/java/com/orange/serviceImpl/user/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.orange.serviceImpl.user;
+
+import com.orange.service.user.UserService;
+
+/**
+ * Created by chzq on 2017/3/30.
+ */
+public class UserServiceImpl implements UserService {
+}
